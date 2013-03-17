52991fabf9bc07f74970539f79d7badaf13e376d
Alex Berry <highwaybobbery@gmail.com>
2013-03-17 00:11:31 -0400

    update styles and text

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,4 +1,4 @@
 PDFKit.configure do |config|
-  config.default_options.merge!({margin_top: '0.25in', margin_bottom: '0.25in'})
+  config.default_options.merge!({margin_top: '0.2in', margin_bottom: '0.2in', margin_left: '0.25in', margin_right: '0.25in'})
 
 end
